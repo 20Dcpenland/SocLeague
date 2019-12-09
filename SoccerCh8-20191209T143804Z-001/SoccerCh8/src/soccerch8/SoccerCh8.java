6ffe2bf6a36e4ba4579ce36a2ee1b9990d317d48
Brentyn Morgan <35702052+20Dcpenland@users.noreply.github.com>
2019-12-09 10:37:34 -0600

    done

---
+++ SoccerCh8-20191209T143804Z-001/SoccerCh8/src/soccerch8/SoccerCh8.java
@@ -0,0 +1,10 @@
+package soccerch8;
+public class SoccerCh8 {
+    public static void main(String[] args) {
+        Schedual sched = new Schedual();
+        sched.startProject();
+    }
+}
+
+    
+
